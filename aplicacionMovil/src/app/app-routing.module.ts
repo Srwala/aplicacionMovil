d0636f11dce66a07a73a9caf3a18c4cda1b78a67
Srwala <el.guala@duocuc.cl>
2024-09-13 12:23:25 -0300

    Ultimate

--- aplicacionMovil/src/app/app-routing.module.ts
+++ aplicacionMovil/src/app/app-routing.module.ts
@@ -22,6 +22,10 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
+  {
+    path: 'qr',
+    loadChildren: () => import('./qr/qr.module').then( m => m.QrPageModule)
+  },
 
 ];
 
